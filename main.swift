ce5f196c1e196f73ccfd47992326802a2a0f6c01
Luis Costa <lmbcosta@hotmail.com>
2016-08-24 17:02:56 +0100

    first commit

---
+++ main.swift
@@ -0,0 +1,55 @@
+#!/usr/bin/swift
+//
+//  main.swift
+//  TextMessage
+//
+//  Created by Luis  Costa on 24/08/16.
+//  Copyright © 2016 Luis  Costa. All rights reserved.
+//
+
+import Foundation
+
+// Collecting textMessage and receiver phone number
+func collectData() -> (String?, String?) {
+    // Message to send
+    print("write your text message")
+    let msgClient = readLine(stripNewline: true)
+    
+    // Collect the phone number
+    print("Write the phone number that you want to send this message")
+    let phoneNumber = readLine(stripNewline: true)
+    
+    return (msgClient, phoneNumber)
+}
+
+func sendTextMessage(msgClient: String?, phoneNumber: String?) {
+    // Checking the optionals
+    if let textMessageClient = msgClient, phoneNumberTextMessage = phoneNumber {
+        // API URL
+        let clockworkAPIURL = "https://api.clockworksms.com/http/send.aspx?"
+        // API key: You can get your API key 
+        // as soon as you sign up in clockworksms.com
+        let clockworkAPIkey = "insert_your_own_clockworksms_API_key"
+        
+        // URL string
+        var urlString = clockworkAPIURL
+            + "key=" + clockworkAPIkey
+            + "&to=" + phoneNumberTextMessage
+            + "&content=" + textMessageClient
+        urlString = urlString.stringByReplacingOccurrencesOfString(" ", withString: "+")
+        print(urlString)
+        
+        //NSURL
+        if let clockworkSMSURL = NSURL(string: urlString),
+            responseData = NSData(contentsOfURL: clockworkSMSURL) {
+            let responseString = String(data: responseData, encoding: NSUTF8StringEncoding)
+            print(responseString)
+        }
+    }
+}
+
+//main
+let data = collectData()
+sendTextMessage(data.0, phoneNumber: data.1)
+
+
